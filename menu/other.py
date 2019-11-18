2b2f7af37a32ca11d845661c14e9b9e7fcd61e85
Salis Mazaya <50546233+salism3@users.noreply.github.com>
2019-11-18 22:16:31 +0700

    Add files via upload

--- menu/other.py
+++ menu/other.py
@@ -7,3 +7,63 @@ def hapus_msg():
 	print()
 	echo("[+] Done!")
 	enter()
+
+def downloader():
+	try: os.mkdir('output')
+	except: pass
+	os.system('clear')
+	logo(n=True)
+	menu = Menu()
+	gas = Other()
+	menu = menu.m8()
+	if menu == 0:
+		m = Menu()
+		m.m7()
+	elif menu == 1:
+		os.system('clear')
+		logo(n=True)
+		echo("[+] Select Your Album To Download")
+		gas.tampilkan_album()
+		tempat = gas.album.split("/")[-2]
+		os.chdir('output')
+		try: os.mkdir(tempat)
+		except: pass
+		os.chdir('..')
+		download_proces(gas.album, f"output/{tempat}")
+	elif menu == 2:
+		url = str(input("   [?] Album Url (use mbasic fb): "))
+		if not 'mbasic.facebook.com' in url:
+			echo("[+] Url Not Valid")
+			enter()
+		elif not 'https://' in url or 'http://' in url:
+			echo("[+] Url Not Valid")
+			enter()
+		data = gas.o_url(url)
+		nama = str(parser(data, 'html.parser').find('title')).replace('<title>', '').replace('</title>', '')
+		if 'Konten Tidak Ditemukan' == nama:
+			echo("[+] Album Not Found")
+			enter()
+		echo("[+] Album Name: " + nama)
+		tempat = url.split("/")[-2]
+		os.chdir('output')
+		try: os.mkdir(tempat)
+		except: pass
+		os.chdir('..')
+		download_proces(url, f"output/{tempat}")
+		
+	else:
+		downloader()
+		
+
+def download_proces(url, path):
+	gas = Other()
+	gas.dump_sts_wclass(url, True, 'Lihat Foto Lainnya', 0, None, href_na=True, filter=False)
+	gas.id = filter(lambda x: "photo.php?" in x or "photos" in x, gas.id)
+	gas.id = list(gas.id)
+	echo("[+] Total: " + str(len(gas.id)))
+	#print(gas.id)
+	gas.download(path)
+	print()
+	echo(f"[+] Done! photos saved in {path}")
+	enter()
+		
